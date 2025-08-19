bea37387d08086fec9f54831fa206f5bdd36c40e
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-19 12:47:30 -0500

    feat: add Imagen repository and service for image management

--- src/main/java/com/ecommerce/backend/config/Constant.java
+++ src/main/java/com/ecommerce/backend/config/Constant.java
@@ -8,4 +8,13 @@ public class Constant {
     public static final String GR_ERROR_NO_HANDLER = "The requested resource was not found";
     public static final String GR_ERROR_PARAMETER_TYPE = "The parameter '%s' must be of type '%s'";
     public static final String GR_ERROR_DETAILS = "[%s] %s";
+
+    // tables
+    public static final String TABLE_IMAGEN = "imagen";
+    public static final String TABLE_PRODUCTO = "producto";
+    public static final String TABLE_CATEGORIA = "categoria";
+    public static final String TABLE_USUARIO = "usuario";
+    public static final String TABLE_COTIZACION = "cotizacion";
+    public static final String TABLE_COTIZACION_DETALLE = "cotizacion_detalle";
+
 }
