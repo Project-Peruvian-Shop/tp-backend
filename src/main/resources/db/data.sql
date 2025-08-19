672ddfd3fe2fcc215d166135a50071b5cfcb141c
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-19 12:10:30 -0500

    feat: update database configuration and seed data for users

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO `admins` (`id`,`full_name`, `email`, `password`, `role`) VALUES
-    (1, 'Admin', 'admin', 'adminpassword', 'ROLE_USER');
+insert into usuario (nombre, apellidos, email, passwordd, telefono) values
+('admin', 'admin', 'admin@mail.com', 'admin123', '1234567890'),
+('user', 'User', 'user@mail.com', 'user123', '0987654321');
