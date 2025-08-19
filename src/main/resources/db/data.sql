855ca57b0375740c908575ba8d4daa436e53e0e3
tineojf <92743762+tineodev@users.noreply.github.com>
2025-08-19 12:47:02 -0500

    feat: add initial image data to seed database

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,11 @@
 insert into usuario (nombre, apellidos, email, passwordd, telefono) values
 ('admin', 'admin', 'admin@mail.com', 'admin123', '1234567890'),
 ('user', 'User', 'user@mail.com', 'user123', '0987654321');
+
+insert into imagen (enlace, nombre, alt) values
+('https://tuberiasperuanito.com/img/tuberia-pvc-110.jpg', 'Tubería PVC 110mm', 'tuberia pvc 110mm'),
+('https://tuberiasperuanito.com/img/tuberia-pvc-90.jpg', 'Tubería PVC 90mm', 'tuberia pvc 90mm'),
+('https://tuberiasperuanito.com/img/tuberia-hdpe-63.jpg', 'Tubería HDPE 63mm', 'tuberia hdpe 63mm'),
+('https://tuberiasperuanito.com/img/tuberia-hdpe-160.jpg', 'Tubería HDPE 160mm', 'tuberia hdpe 160mm'),
+('https://tuberiasperuanito.com/img/codo-90.jpg', 'Codo PVC 90°', 'codo pvc 90 grados'),
+('https://tuberiasperuanito.com/img/tee-agua.jpg', 'Tee de agua PVC', 'tee pvc agua');
