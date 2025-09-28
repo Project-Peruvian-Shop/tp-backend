de174cbde9fb4e0ea8e7ad96239abe5257a0234e
tineojf <92743762+tineodev@users.noreply.github.com>
2025-09-28 14:15:24 -0500

    feat: add ProductoFullResponseDTO and mapping method for detailed product response

---
+++ src/main/java/com/ecommerce/backend/dto/producto/ProductoFullResponseDTO.java
@@ -0,0 +1,21 @@
+package com.ecommerce.backend.dto.producto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductoFullResponseDTO {
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+
+    private String productoEnlace;
+    private String productoAlt;
+
+    private String categoria;
+    private String categoriaEnlace;
+    private String categoriaAlt;
+    private String categoriaUsos;
+}
--- src/main/java/com/ecommerce/backend/mapper/ProductoMapper.java
+++ src/main/java/com/ecommerce/backend/mapper/ProductoMapper.java
@@ -1,10 +1,10 @@
 package com.ecommerce.backend.mapper;
 
 import com.ecommerce.backend.dto.producto.PaginatedProductoResponseDTO;
+import com.ecommerce.backend.dto.producto.ProductoFullResponseDTO;
 import com.ecommerce.backend.entity.Imagen;
 import com.ecommerce.backend.entity.Producto;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class ProductoMapper {
@@ -25,6 +25,31 @@ public class ProductoMapper {
                 .nombre(producto.getNombre())
                 .imagenUrl(producto.getImagen().getEnlace())
                 .imagenAlt(producto.getImagen().getAlt())
+                .build();
+    }
+
+    public static ProductoFullResponseDTO toDTOGetByID(Producto producto) {
+        if (producto == null) {
+            return null;
+        }
+
+        if (producto.getImagen() == null) {
+            Imagen imagen = new Imagen();
+            imagen.setEnlace("NOT_FOUND");
+            imagen.setAlt("NOT_FOUND");
+            producto.setImagen(imagen);
+        }
+
+        return ProductoFullResponseDTO.builder()
+                .id(producto.getId())
+                .nombre(producto.getNombre())
+                .descripcion(producto.getDescripcion())
+                .productoEnlace(producto.getImagen().getEnlace())
+                .productoAlt(producto.getImagen().getAlt())
+                .categoria(producto.getCategoria().getNombre() + " " + producto.getCategoria().getNorma())
+                .categoriaEnlace(producto.getCategoria().getImagen().getEnlace())
+                .categoriaAlt(producto.getCategoria().getImagen().getAlt())
+                .categoriaUsos(producto.getCategoria().getUsos())
                 .build();
     }
 }
