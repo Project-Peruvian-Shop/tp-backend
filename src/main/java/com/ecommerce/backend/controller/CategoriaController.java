d58a79079551fb00790da51d0d50af04e3ce5305
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-05 01:05:36 -0500

    feat: refactor CategoriaController to simplify response handling and improve clarity

--- src/main/java/com/ecommerce/backend/controller/CategoriaController.java
+++ src/main/java/com/ecommerce/backend/controller/CategoriaController.java
@@ -2,16 +2,22 @@ package com.ecommerce.backend.controller;
 
 import com.ecommerce.backend.config.Constant;
 import com.ecommerce.backend.dto.GlobalResponse;
+import com.ecommerce.backend.dto.categoria.AllAndQuantityResponseDTO;
+import com.ecommerce.backend.dto.categoria.CategoriaDashboardResponseDTO;
 import com.ecommerce.backend.dto.categoria.CategoriaRequestDTO;
+import com.ecommerce.backend.dto.producto.PaginatedProductoResponseDTO;
 import com.ecommerce.backend.service.CategoriaService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -26,30 +32,11 @@ public class CategoriaController {
             description = "Ubicación: Side bar en tienda  \n" +
                     "Seguridad: Pública"
     )
-    public ResponseEntity<GlobalResponse> findAllAndQuantity() {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+    public ResponseEntity<GlobalResponse<List<AllAndQuantityResponseDTO>>> findAllAndQuantity() {
+        List<AllAndQuantityResponseDTO> data = categoriaService.findAllAndQuantity();
 
-        try {
-            data = categoriaService.findAllAndQuantity();
-            message = "Categorias retrieved successfully";
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "An error occurred while retrieving categorias";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GlobalResponse.success(data, "Categorias obtenidas exitosamente"));
     }
 
 
@@ -59,30 +46,11 @@ public class CategoriaController {
             description = "Ubicación: Dashboard - Categorias  \n" +
                     "Seguridad: Admin, Manager"
     )
-    public ResponseEntity<GlobalResponse> countAllCategorias() {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+    public ResponseEntity<GlobalResponse<Long>> countAllCategorias() {
+        Long data = categoriaService.countAllCategorias();
 
-        try {
-            data = categoriaService.countAllCategorias();
-            message = "Cantidad de categorias para dashboard";
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "Error al traer cantidad de categorias";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GlobalResponse.success(data, "Cantidad de categorias para dashboard"));
     }
 
 
@@ -92,34 +60,15 @@ public class CategoriaController {
             description = "Ubicación: Dashboard - Categorias  \n" +
                     "Seguridad: Admin, Manager"
     )
-    public ResponseEntity<GlobalResponse> getAllPaginatedDashboard(
+    public ResponseEntity<GlobalResponse<Page<CategoriaDashboardResponseDTO>>> getAllPaginatedDashboard(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CategoriaDashboardResponseDTO> data = categoriaService.findAllPaginatedDashboard(pageable);
 
-        try {
-            Pageable pageable = PageRequest.of(page, size);
-            data = categoriaService.findAllPaginatedDashboard(pageable);
-            message = "Paginated Categorias para dashboard";
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "An error occurred while retrieving paginated categorias";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK).
+                body(GlobalResponse.success(data, "Categorias paginadas para dashboard"));
     }
 
 
@@ -129,36 +78,18 @@ public class CategoriaController {
             description = "Ubicación: Dashboard - Categorias  \n" +
                     "Seguridad: Admin, Manager"
     )
-    public ResponseEntity<GlobalResponse> searchByNombreUsosNorma(
+    public ResponseEntity<GlobalResponse<Page<CategoriaDashboardResponseDTO>>> searchByNombreUsosNorma(
             @RequestParam(defaultValue = "") String busqueda,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CategoriaDashboardResponseDTO> data = categoriaService.searchByNombreUsosNorma(busqueda, pageable);
 
-        try {
-            Pageable pageable = PageRequest.of(page, size);
-            data = categoriaService.searchByNombreUsosNorma(busqueda, pageable);
-            message = "Busqueda de Categorias para dashboard";
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "An error occurred while retrieving search categorias";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK).
+                body(GlobalResponse.success(data, "Categorias buscadas para dashboard"));
     }
+
 
     @GetMapping("/{id}")
     @Operation(
@@ -166,30 +97,11 @@ public class CategoriaController {
             description = "Ubicación: Dashboard categoria one  \n" +
                     "Seguridad: Admin, Manager"
     )
-    public ResponseEntity<GlobalResponse> getByID(@PathVariable Long id) {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+    public ResponseEntity<GlobalResponse<CategoriaDashboardResponseDTO>> getByID(@PathVariable Long id) {
+        CategoriaDashboardResponseDTO data = categoriaService.findByID(id);
 
-        try {
-            data = categoriaService.findByID(id);
-            message = "Categoria con id " + id;
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "Error al traer el Categoria";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GlobalResponse.success(data, "Categoria encontrada con id: " + id));
     }
 
 
@@ -199,35 +111,16 @@ public class CategoriaController {
             description = "Ubicación: Dashboard categoria one & landing  \n" +
                     "Seguridad: Admin, Manager, public"
     )
-    public ResponseEntity<GlobalResponse> getProductosByID(
+    public ResponseEntity<GlobalResponse<Page<PaginatedProductoResponseDTO>>> getProductosByID(
             @PathVariable Long id,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PaginatedProductoResponseDTO> data = categoriaService.findProductosByCategoriaID(id, pageable);
 
-        try {
-            Pageable pageable = PageRequest.of(page, size);
-            data = categoriaService.findProductosByCategoriaID(id, pageable);
-            message = "Productos de la categoria " + id;
-            status = HttpStatus.OK;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "Error al traer los productos de la categoria";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GlobalResponse.success(data, "Productos encontrados para categoria con id: " + id));
     }
 
 
@@ -237,32 +130,12 @@ public class CategoriaController {
             description = "Ubicación: Dashboard - Categorias  \n" +
                     "Seguridad: Admin, Manager"
     )
-    public ResponseEntity<GlobalResponse> save(@Valid @RequestBody CategoriaRequestDTO categoria) {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+    public ResponseEntity<GlobalResponse<CategoriaDashboardResponseDTO>> save(@Valid @RequestBody CategoriaRequestDTO categoria) {
+        CategoriaDashboardResponseDTO data = categoriaService.save(categoria);
 
-        try {
-            data = categoriaService.save(categoria);
-            message = "Categoria creada";
-            status = HttpStatus.CREATED;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "Error al crear categoria";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(GlobalResponse.success(data, "Categoria creada exitosamente"));
     }
-
 
     @PutMapping("/{id}")
     @Operation(
@@ -270,32 +143,13 @@ public class CategoriaController {
             description = "Ubicación: Dashboard - Categorias  \n" +
                     "Seguridad: Admin, Manager"
     )
-    public ResponseEntity<GlobalResponse> update(
+    public ResponseEntity<GlobalResponse<CategoriaDashboardResponseDTO>> update(
             @PathVariable Long id,
             @Valid @RequestBody CategoriaRequestDTO categoria
     ) {
-        HttpStatus status;
-        Object data = null;
-        String message;
-        String details = null;
+        CategoriaDashboardResponseDTO data = categoriaService.update(id, categoria);
 
-        try {
-            data = categoriaService.update(id, categoria);
-            message = "Categoria actualizada";
-            status = HttpStatus.CREATED;
-        } catch (Exception e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            message = "Error al actualizar categoria";
-            details = e.getMessage();
-        }
-
-        return ResponseEntity.status(status).body(
-                GlobalResponse.builder()
-                        .ok(data != null)
-                        .message(message)
-                        .data(data)
-                        .details(details)
-                        .build()
-        );
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GlobalResponse.success(data, "Categoria actualizada exitosamente"));
     }
 }
