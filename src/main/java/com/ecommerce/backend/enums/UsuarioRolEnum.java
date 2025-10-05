69ec45e3a759771adfccb43d3d5ae6db14641ef1
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-04 23:53:36 -0500

    feat: add authentication DTOs and mapper for user registration and login

---
+++ src/main/java/com/ecommerce/backend/enums/UsuarioRolEnum.java
@@ -0,0 +1,7 @@
+package com.ecommerce.backend.enums;
+
+public enum UsuarioRolEnum {
+    ROLE_ADMIN,
+    ROLE_MANAGER,
+    ROLES_USER
+}
---
+++ src/main/java/com/ecommerce/backend/service/AuthService.java
@@ -0,0 +1,48 @@
+package com.ecommerce.backend.service;
+
+import com.ecommerce.backend.dto.auth.LoginRequestDTO;
+import com.ecommerce.backend.dto.auth.LoginResponseDTO;
+import com.ecommerce.backend.dto.auth.RegisterRequestDTO;
+import com.ecommerce.backend.dto.auth.RegisterResponseDTO;
+import com.ecommerce.backend.dto.usuario.UsuarioResponseDTO;
+import com.ecommerce.backend.entity.Usuario;
+import com.ecommerce.backend.enums.UsuarioRolEnum;
+import com.ecommerce.backend.exceptions.EntityAlreadyExistsException;
+import com.ecommerce.backend.exceptions.InvalidCredentialsException;
+import com.ecommerce.backend.exceptions.ResourceNotFoundException;
+import com.ecommerce.backend.mapper.AuthMapper;
+import com.ecommerce.backend.mapper.UsuarioMapper;
+import com.ecommerce.backend.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UsuarioRepository usuarioRepository;
+
+    public RegisterResponseDTO register(RegisterRequestDTO registerRequestDTO) {
+        if (usuarioRepository.existsByEmail(registerRequestDTO.getEmail())) {
+            throw new EntityAlreadyExistsException("El email " + registerRequestDTO.getEmail() + " ya está en uso.");
+        }
+        Usuario usuario = AuthMapper.toEntity(registerRequestDTO);
+        usuario.setRol(UsuarioRolEnum.ROLES_USER);
+        Usuario savedUsuario = usuarioRepository.save(usuario);
+
+        return AuthMapper.toDTO(savedUsuario);
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
+        String email = loginRequestDTO.getEmail();
+        String passwordd = loginRequestDTO.getPasswordd();
+
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con el email: " + email));
+
+        if (!usuario.getPasswordd().equals(passwordd)) {
+            throw new InvalidCredentialsException("Contraseña incorrecta, intente de nuevo.");
+        }
+        return ;
+    }
+}
