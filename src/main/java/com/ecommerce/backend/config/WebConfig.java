da2df11913981208a178c8b3ffafcf11f4132e51
joaoDevL <joao.software.dev@gmail.com>
2025-10-15 23:38:44 -0500

    refactor: Configuring to allow us to bring the URL of the files to the front

--- src/main/java/com/ecommerce/backend/config/WebConfig.java
+++ src/main/java/com/ecommerce/backend/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.ecommerce.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,6 +18,13 @@ public class WebConfig {
                         .allowedOrigins("http://localhost:5173") // frontend vite
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
+            }
+            // Configuración para mostrar archivos estáticos desde el sistema de archi vos
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                // Sirve archivos desde la carpeta 'uploads' en el sistema de archivos
+                registry.addResourceHandler("/uploads/**")
+                        .addResourceLocations("file:uploads/");
             }
         };
     }
